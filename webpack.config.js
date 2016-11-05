3fc6e2c69552b8ad7f1922de8c6ead99b04a248a
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-11-05 14:52:21 -0700

    added dependencies and created basic webpack.config file scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const webpack = require('webpack');
+const ExtractText = require('extract-webpack-text-plugin');
+const autoprefixer = require('autoprefixer');
+
+// const API_URL = ''
+
+let plugins = [
+  new ExtractText('bundle.css')
+  // new webpack.DefinePlugin({__API_URL__: API_URL})
+];
+
+module.exports = {
+  entry: `${__dirname}/app/entry.js`,
+  plugins: plugins,
+  output: {
+    path: 'build',
+    filename: 'bundle.js'
+  },
+  postcss: function() {
+    return [autoprefixer];
+  },
+  sassLoader: {
+    includePaths: [`${__dirname}/app/scss/lib`]
+  },
+  modules: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractText.plugin('style', 'css!postcss!sass!')
+      },
+      {
+        test: /\.html$/,
+        loader: 'html',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: '/node_modules/',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
