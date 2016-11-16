9d4942f830c9219bcbb8a8cac94c22e56931ed74
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-11-15 19:13:08 -0800

    moving towards using hamburger menu. introduced angular-route

--- app/entry.js
+++ app/entry.js
@@ -4,6 +4,17 @@ require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/base.scss');
 
 const angular = require('angular');
-let golfApp = angular.module('golfApp', []);
+let golfApp = angular.module('golfApp', [require('angular-route')]);
 
 require('./components')(golfApp);
+require('./controllers')(golfApp);
+
+golfApp.config(['$routeProvider', ($rp) => {
+  $rp
+    .when('/home', {
+      template: require('./html/home.html')
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
+}]);
