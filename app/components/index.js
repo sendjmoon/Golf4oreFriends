b341d5a3d7cf81b103b43d1aed06555f52d82df2
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-11-16 23:58:05 -0800

    basic groundwork for having leaderboard at foot of page

--- app/components/index.js
+++ app/components/index.js
@@ -2,5 +2,6 @@
 
 module.exports = function(app) {
   require('./nav-bar')(app);
-  require('./leader-ticker')(app);
+  // require('./leader-ticker')(app);
+  require('./leaderboard-ticker')(app);
 };
