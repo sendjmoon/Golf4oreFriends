72857a1925f0cecaa796bfdcd5cabd8e98edde08
JAMES MOON <SENDJMOON@GMAIL.COM>
2017-02-17 15:43:15 -0800

    prepping for deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(`${__dirname}/build`));
+
+app.listen(port, function(){
+  console.log('server up on', port);
+});
